81124bfdccd9327fafebf2870110d5b64f31a608
unknown <kasha.yq@hz.ali.com>
2019-12-03 09:49:11 +0800

    cnn resize

--- 02_cnn3.py
+++ 02_cnn3.py
@@ -69,7 +69,7 @@ test_images = test_images / 255.0
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
                  activation='relu',
-                 input_shape=(300,300)))
+                 input_shape=(300,300,3)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
